69f8defbcfd59c24ce1b307e32ba57598b35f33e
PedrumK <pedrumkholghi@gmail.com>
2021-02-19 16:26:57 +0000

    Updated submit button to open login.html

--- handpicked_flask/app/app.py
+++ handpicked_flask/app/app.py
@@ -1,18 +1,17 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/', methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
     if request.method == "POST":
-        # req = request.form
-        # username = req["username"]
-        # password = request.form["password"]
+        if request.form['username'] != 'admin': 
+             username = request.form["username"]
+             # password = request.form["password"]      
         return render_template("login.html")
+        # password = request.form["password"]
+    else:
+        return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
